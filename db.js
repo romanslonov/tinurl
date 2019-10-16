ffc58aa96b0a346e0dcad50258e6e87cbe3f165c
Roman <destabiliseus@gmail.com>
2019-10-16 13:39:15 +0800

    Added port var to db connection

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const pool = mysql2.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    connectionLimit: 100
+    connectionLimit: 100,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool.promise();
