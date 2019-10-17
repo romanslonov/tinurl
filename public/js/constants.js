71e32834838b8c29e78289e66286b2fec6f392cd
Roman <destabiliseus@gmail.com>
2019-10-17 12:04:00 +0800

    Removed slash in hostname

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,3 +1,3 @@
 export const ENV = process.env.NODE_ENV;
 export const ENV_PRODUCTION = ENV === 'production';
-export const HOSTNAME = ENV_PRODUCTION ? 'https://tinurl.cc/' : 'http://localhost:9000';
+export const HOSTNAME = ENV_PRODUCTION ? 'https://tinurl.cc' : 'http://localhost:9000';
